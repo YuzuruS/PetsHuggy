dacf741c9ea67fa6beacab2702b729115b855176
YuzuruS <navitima@gmail.com>
2017-04-24 22:24:08 +0900

    part4-3-2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" , :registrations => 'registrations'}
   resources :users, only: [:show]
+
+  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing_basics'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
